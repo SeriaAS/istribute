4d31992d6ff66072e27c330ee7836a1d97a44dff
JanespenSeria <jan-espen@seria.no>
2014-09-04 09:39:43 +0200

    Add appkeys view with ability to set url/appid/appkey.

--- framework/joomla/administrator/components/com_istribute/controller.php
+++ framework/joomla/administrator/components/com_istribute/controller.php
@@ -6,11 +6,46 @@ class IstributeController extends JControllerLegacy {
 	public function display($cachable = false, $urlparams = false)
 	{
 		JPluginHelper::importPlugin('content');
+		$app =& JFactory::getApplication();
+		$config = JComponentHelper::getParams('com_istribute');
+
+		if (isset($_POST['istributeUrl'])) {
+			$istributeUrl = $_POST['istributeUrl'];
+			$istributeAppId = $_POST['istributeAppId'];
+			$istributeAppKey = $_POST['istributeAppKey'];
+			$config->set('istribute.api.url', $istributeUrl);
+			$config->set('istribute.api.appid', $istributeAppId);
+			$config->set('istribute.api.appkey', $istributeAppKey);
+			$db = JFactory::getDBO();
+			$query = $db->getQuery(true);
+
+// Build the query
+			$query->update('#__extensions AS a');
+			$query->set('a.params = ' . $db->quote((string)$config));
+			$query->where('a.element = "com_istribute"');
+
+// Execute the query
+			$db->setQuery($query);
+			$db->execute();
+		} else {
+			$istributeUrl = $config->get('istribute.api.url', 'https://api.istribute.com');
+			$istributeAppId = $config->get('istribute.api.appid', '');
+			$istributeAppKey = $config->get('istribute.api.appkey', '');
+		}
+
 		$vName = $this->input->get('view', 'videos');
 		switch ($vName)
 		{
+			case 'appkeys':
+				if ($istributeUrl && $istributeAppId && $istributeAppKey)
+					$app->redirect('index.php?option=com_istribute&view=videos');
+				$vLayout = $this->input->get('layout', 'default', 'string');
+				$mName = 'manager';
+				break;
 			case 'videos':
 			default:
+				if (!$istributeUrl || !$istributeAppId || !$istributeAppKey)
+					$app->redirect('index.php?option=com_istribute&view=appkeys');
 				$vLayout = $this->input->get('layout', 'default', 'string');
 				$mName = 'manager';
 
